526838fd16776fa95f66569c24ea5f3c1dca94c6
NathanAlcantara <nathan.alcantara@senior.com.br>
2022-09-28 21:26:47 +0000

    Added HMR

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,5 +14,10 @@ async function bootstrap() {
   const port = configService.get('PORT');
 
   await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
